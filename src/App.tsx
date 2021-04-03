a8b4963af1d4d0c3b5271af62b92e0bbbddb97af
Rafael Sequeiros <rafanetow@gmail.com>
2021-04-02 23:46:21 -0600

    working on componen tournamentCreate

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import React, {useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { useState } from 'react';
 import './App.css';
 import { TournamentList } from './components/tournament/tournamentList';
 import { TournamentCreate } from './components/tournament/tournamentCreate';
@@ -7,49 +13,47 @@ import { Layout, Menu, Breadcrumb } from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+
 
 interface ComponentDictionary {
   [name: string]: any;
 }
 const mainComponents: ComponentDictionary = {
- tournamentList:<TournamentList/>,
- tournamentCreate:<TournamentCreate/>
+  tournamentList: <TournamentList />,
+  tournamentCreate: <TournamentCreate />
 };
 
 function App() {
   const [currentComponent, setCurrentComponent] = useState("..");
   return (
-    
+
     <div className="App">
+      <Router>
       <Layout>
-    <Header className="header">
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">Saiymant Aplication</Menu.Item>
-      </Menu>
-    </Header>
-    <Layout>
-      <Sider width={200} className="site-layout-background">
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          style={{ height: '100%', borderRight: 0 }}
-        >
-          <SubMenu key="sub1" icon={<UserOutlined />} title="Tournament">
-            <Menu.Item key="1" onClick={() => setCurrentComponent("tournamentCreate")}>Create Tournament</Menu.Item>
-            <Menu.Item key="2" onClick={() => setCurrentComponent("tournamentList")}>List Tournaments</Menu.Item>
-          </SubMenu>
-        </Menu>
-      </Sider>
-      <Layout style={{ padding: '0 24px 24px' }}>
-        <Breadcrumb style={{ margin: "16px 0" }}></Breadcrumb>
+        <Header className="header">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">Saiymant Aplication</Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Sider width={200} className="site-layout-background">
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%', borderRight: 0 }}
+            >
+              <SubMenu key="sub1" icon={<UserOutlined />} title="Tournament">
+             
+                  <Menu.Item key="1" onClick={() => setCurrentComponent("tournamentCreate")}>   <Link to="/tournamentCreate"><span>Create Tournament</span></Link></Menu.Item>
+                  <Menu.Item key="2" onClick={() => setCurrentComponent("tournamentList")}><Link to="/tournamentList"><span>List Tournament</span></Link></Menu.Item>
+   
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: "16px 0" }}></Breadcrumb>
             <Content
               style={{
                 background: "#fff",
@@ -58,18 +62,19 @@ function App() {
                 minHeight: 500
               }}
             >
-             <Switch>
-            <Route path="/password-forget-form">
-              <TournamentList />
-            </Route>
-            <Route path="/next-item">
-              <TournamentCreate component to render in the content div />
-            </Route>
-          </Switch>
+              <Switch>
+                <Route path="/tournamentList">
+                  <TournamentList />
+                </Route>
+                <Route path="/tournamentCreate">
+                  <TournamentCreate component to render in the content div />
+                </Route>
+              </Switch>
             </Content>
+          </Layout>
+        </Layout>
       </Layout>
-    </Layout>
-  </Layout>
+      </Router>
     </div>
   );
 }
--- src/components/tournament/tournamentCreate.tsx
+++ src/components/tournament/tournamentCreate.tsx
@@ -1,32 +1,17 @@
 import React, { ChangeEvent, MouseEventHandler } from "react";
 import "antd/dist/antd.css";
 import { Card, Avatar, Form, Input, Button } from 'antd';
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
 import Meta from "antd/lib/card/Meta";
+import { useHistory } from "react-router-dom";
 import TournamentCreateModel from "./modelCreate";
-export class TournamentCreate extends React.Component<any, any> {
-  state: TournamentCreateModel = {
-    name: " "
-  };
-
-  handleChangeTournament = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    let name = event.target.value
-    this.setState({
-      name
-    });
-    console.log(this.state)
-  };
-
-  handleChangeName = (name: String) => {
-    this.setState({
-      name
-    });
-  };
-
-   saveTournament = ()  => {
+function TournamentCreate() {  
+  const [name, setCurrentName] = useState("..");
+  /*saveTournament = ()  => {
+    let history = useHistory();
     let request: any = {};
-    request.name =this.state.name
+    request.name =name
     console.log(JSON.stringify(request));
     fetch('http://localhost:4000/Tournament', {
       method: 'POST', // or 'PUT'
@@ -37,14 +22,14 @@ export class TournamentCreate extends React.Component<any, any> {
     })
       .then(response => response.json())
       .then(data => {
+        history.push("/home");
         console.log('Success:', data);
       })
       .catch((error) => {
         console.error('Error:', error);
       });
-  }
-  render() {
-    const { TextArea } = Input;
+  }*/
+  const { TextArea } = Input;
 
    
 
@@ -88,11 +73,11 @@ export class TournamentCreate extends React.Component<any, any> {
               }
             ]}
           >
-              <TextArea onChange={e => this.handleChangeTournament((e))} />
+              <TextArea onChange={e => setCurrentName((e.target.value))} />
             </Form.Item>
 
           <Form.Item {...tailLayout}>
-            <Button type="primary" htmlType="submit" onClick={this.saveTournament}>
+            <Button type="primary" htmlType="submit" >
               Submit
         </Button>
           </Form.Item>
@@ -101,4 +86,3 @@ export class TournamentCreate extends React.Component<any, any> {
 
     );
   }
-}
